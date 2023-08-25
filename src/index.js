3078aea2a6ed73c5ced6886b9848df0a341efed9
ncharlemagne <125274331+ncharlemagne@users.noreply.github.com>
2023-08-25 16:56:51 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>My Shopping List</h1>
+    <ul>
+      <li>Coffee</li>
+      <li>Tea</li>
+      <li>Milk</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
+//It should display a h1 heading.
+//It should display an unordered list (bullet points).
+//It should contain 3 list elements.
